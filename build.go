5fa52dd922cc13bdbc5aca43ff17d94790fef37b
Jon Barber <jon.barber@acm.org>
2023-02-10 10:38:10 +0000

    Add deploy step

--- build.go
+++ build.go
@@ -27,6 +27,7 @@ func main() {
 		os.Exit(1)
 	}
 
+	// write contents of container build/ directory to the host
 	for _, d := range built {
 		_, err = d.Export(ctx, "build")
 		if err != nil {
@@ -35,11 +36,30 @@ func main() {
 		}
 
 	}
-	// write contents of container build/ directory to the host
-	//_, err = output.Export(ctx, "build")
-	//if err != nil {
-	//	return err
-	//}
+
+	if err := deploy(ctx, client.Pipeline("Deploy")); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+}
+
+func deploy(ctx context.Context, client *dagger.Client) error {
+
+	// get `node` image
+	node := client.Container().From("node:lts-gallium").
+		WithEnvVariable("AWS_ACCESS_KEY_ID", os.Getenv("AWS_ACCESS_KEY_ID")).
+		WithEnvVariable("AWS_SECRET_ACCESS_KEY", os.Getenv("AWS_SECRET_ACCESS_KEY")).
+		WithEnvVariable("AWS_SESSION_TOKEN", os.Getenv("AWS_SESSION_TOKEN")).
+		WithEnvVariable("AWS_REGION", os.Getenv("AWS_REGION")).
+		WithMountedDirectory("/src", client.Host().Directory(".")).
+		WithWorkdir("/src")
+
+	exitCode, err := node.WithExec([]string{"npm", "install"}).WithExec([]string{"npx", "sls", "deploy", "--verbose"}).ExitCode(ctx)
+	// being executed on
+	fmt.Printf("npx says: %d\n", exitCode)
+
+	return err
 
 }
 
@@ -63,7 +83,7 @@ func build(ctx context.Context, client *dagger.Client) ([]*dagger.Directory, err
 		WithEnvVariable("GOMODCACHE", "/cache").
 		WithEnvVariable("GOOS", "linux").
 		WithEnvVariable("GOARCH", "amd64").
-		WithMountedDirectory("/src", client.Host().Directory(".")).WithWorkdir("/src").
+		WithMountedDirectory("/src", client.Host().Directory(".")).
 		WithWorkdir("/src")
 
 	packager := client.Container().From("alpine:3").
@@ -100,83 +120,6 @@ func build(ctx context.Context, client *dagger.Client) ([]*dagger.Directory, err
 	}
 	return builds, err
 
-}
-
-func oldbuild(ctx context.Context) error {
-	fmt.Println("Building with Dagger")
-
-	fns, err := getFunctionNames()
-	if err != nil {
-		return err
-	}
-
-	// initialize Dagger client
-	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-
-	// get reference to the local project
-	src := client.Host().Directory(".")
-
-	// get `golang` image
-	golang := client.Container().From("golang:latest")
-
-	// mount cloned repository into `golang` image
-	golang = golang.WithMountedDirectory("/src", src).WithWorkdir("/src")
-
-	// set build variables
-	golang = golang.WithEnvVariable("GOOS", "linux")
-	golang = golang.WithEnvVariable("GOARCH", "amd64")
-
-	var binaries []string
-	for _, fn := range fns {
-		// define the application build command
-		path := fmt.Sprintf("build/%s/bootstrap", fn)
-		//zip := fmt.Sprintf("build/%s.zip", fn)
-
-		golang = golang.WithExec([]string{"go", "build", "-tags", "lambda.norpc", "-ldflags", "-s -w", "-o", path, "tlsposture/functions/" + fn})
-		binaries = append(binaries, fn)
-	}
-
-	// Package up the binaries into zips
-	// get archiver
-	archiver := client.Container().From("alpine:3").
-		WithMountedDirectory("/src/build", golang.Directory("/src/build")).
-		WithWorkdir("/src").
-		WithExec([]string{"apk", "update"}).
-		WithExec([]string{"apk", "add", "zip"})
-
-	for _, binary := range binaries {
-
-		archiver = archiver.WithExec([]string{"zip", "-j", fmt.Sprintf("build/%s.zip", binary), fmt.Sprintf("build/%s/bootstrap", binary)})
-	}
-
-	// get reference to build output directory in container
-	output := archiver.Directory("build")
-
-	// write contents of container build/ directory to the host
-	_, err = output.Export(ctx, "build")
-	if err != nil {
-		return err
-	}
-
-	// get `node` image
-	node := client.Container().From("node:lts-gallium").
-		WithMountedDirectory("/src/build", archiver.Directory("/src/build")).
-		WithEnvVariable("AWS_ACCESS_KEY_ID", os.Getenv("AWS_ACCESS_KEY_ID")).
-		WithEnvVariable("AWS_SECRET_ACCESS_KEY", os.Getenv("AWS_SECRET_ACCESS_KEY")).
-		WithEnvVariable("AWS_SESSION_TOKEN", os.Getenv("AWS_SESSION_TOKEN")).
-		WithEnvVariable("AWS_REGION", os.Getenv("AWS_REGION"))
-
-	// mount cloned repository into `golang` image
-	node = node.WithMountedDirectory("/src", src).WithWorkdir("/src")
-	exitCode, err := node.WithExec([]string{"npm", "install"}).WithExec([]string{"npx", "sls", "deploy", "--verbose"}).ExitCode(ctx)
-	// being executed on
-	fmt.Printf("npx says: %d\n", exitCode)
-
-	return nil
 }
 
 func getFunctionNames() ([]string, error) {
